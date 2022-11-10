c2026c394b252589ef66c348724b362fc88a3d1d
Hjordten <andr314e@stud.kea.dk>
2022-11-10 15:03:05 +0100

    IT FUCKING WORKS! (could be beautified.... Or not)

--- src/main/java/HumanOrNotComparatorFalse.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class HumanOrNotComparatorFalse implements Comparator<Superhero> {
-    @Override
-    public int compare(Superhero o1, Superhero o2) {
-        return Boolean.compare(o1.getSuperheroHumanOrNot() == false,o2.getSuperheroHumanOrNot() == true);
-    }
-}
